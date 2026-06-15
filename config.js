a1920fcd744d3230e7001fe8ce2249207617b661
Lucas Piriz <tijuanatipu@gmail.com>
2026-06-14 21:14:12 -0300

    update: Products list updated

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const PRODUCTOS_CONFIG = [
     { nombre: 'MANDARINAS [POR PESO]', precio: 1500, unidad: 'kilo', peso: '2000 gr', emoji: '🍊' },
     { nombre: 'ACELGA AGROECOLOGICA [ATADO]', precio: 2800, unidad: 'atado', peso: '800 gr', emoji: '🥬' },
     { nombre: 'ACHICORIA [ATADO]', precio: 1200, unidad: 'unidad', peso: '200 gr', emoji: '🥗' },
-    { nombre: 'CALABAZA [POR PESO]', precio: 1500, unidad: 'kilo', peso: '1000 gr', emoji: '🎃' },
+    { nombre: 'CALABAZA [POR PESO]', precio: 1000, unidad: 'kilo', peso: '1000 gr', emoji: '🎃' },
     { nombre: 'CIBULLETE [ATADO]', precio: 1500, unidad: 'atado', peso: '25 gr', emoji: '🧅' },
     { nombre: 'HOJAS ENSALADA [MEZCLUM] X350GR', precio: 4000, unidad: 'unidad', peso: '350 gr', emoji: '🥗' },
     { nombre: 'HUEVOS DE CAMPO [POR 1/2 DOC.]', precio: 2500, unidad: 'unidad', peso: '1/2 docena', emoji: '🥚' },
